c41b8f517f3a32c8d697366d49a1e8fb6b495007
Suraj Kumar <surajksharma2304@gmail.com>
2025-07-24 21:27:33 +0530

    initial commit

---
+++ 01 Learn c++/binaryOperators.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // Arithetic opertors 
+    int a = 10;
+    int b = 5;
+
+    // cout << (a+b) << endl;
+    // cout << (a-b) << endl;
+    // cout << (a*b) << endl;
+    // cout << (a/b) << endl;
+    // cout << (a%b) << endl; // this returns reminder
+
+    // Relational Operators 
+
+    // cout << (a != b) << endl;
+    // cout << (a == b) << endl;
+    // cout << (a > b) << endl;
+    // cout << (a >= b) << endl;
+    // cout << (a < b) << endl;
+    // cout << (a <= b) << endl;
+
+    // Logical Operators 
+    // bool cond1 = true;
+    // bool cond2 = true;
+    // bool cond3 = false;
+
+    // cout << (cond1 && cond2) << endl;
+    // cout << (cond1 || cond2) << endl;
+    // cout << (!cond2) << endl;
+
+
+    // Assingment Operators 
+    // cout << a << endl;
+    // a += 10;
+    // cout << a << endl;
+    // a -= 10;
+    // cout << a << endl;
+    // a *= 10;
+    // cout << a << endl;
+    // a /= 10;
+    // cout << a << endl;
+    // a %= 10;
+    // cout << a << endl;
+
+    // Bitwise Operators 
+
+    cout << (a&b) << endl;
+    cout << (a|b) << endl;
+    cout << (a^b^a) << endl;
+    cout << (a<<b) << endl;
+    cout << (a>>b) << endl;
+
+
+
+    return 0;
+}
