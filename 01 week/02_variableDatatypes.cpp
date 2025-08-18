35eae2cf02e5392d9bd0c9bc61dfc37cccce34e3
Suraj Kumar <surajksharma2304@gmail.com>
2025-08-18 15:51:09 +0530

    variables

--- 01 week/02_variableDatatypes.cpp
+++ 01 week/02_variableDatatypes.cpp
@@ -5,5 +5,20 @@ int main(){
     int age = 25;
 
     cout << "Suraj age is " << age << endl;
+
+    float share = 3.14;
+    char alpha = 'Z';
+    double weight = 56.987;
+    bool isMale = true;
+    bool isFemale = 0;
+
+    cout << share << endl;
+    cout << alpha << endl;
+    cout << weight << endl;
+    cout << isMale << endl;
+    cout << isFemale << endl;
+
+    cout << sizeof(int) * 8 << endl;
+    
     return 0;
 }
