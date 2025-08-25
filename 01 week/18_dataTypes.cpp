1ad9dcde1822439ea5d2d1b1478c3c29f52dee64
Suraj Kumar <surajksharma2304@gmail.com>
2025-08-25 10:52:33 +0530

    learn c++

---
+++ 01 week/18_dataTypes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    // variable initilization 
+    age = 20;
+    char ch = 'C';
+    double d = 12.342423;
+    float f = 3.313;
+    return  0;
+
+}
